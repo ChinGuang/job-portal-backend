38f8197d7f0a8b5a5bf214254d1037db08332243
Chin Guang <ganchinguang0622@gmail.com>
2026-08-24 20:23:47 +0800

    style: format code for consistency in CreateUsersTable migration

--- src/database/migrations/1787573944791-CreateUsersTable.ts
+++ src/database/migrations/1787573944791-CreateUsersTable.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUsersTable1787573944791 implements MigrationInterface {
-    name = 'CreateUsersTable1787573944791'
+  name = 'CreateUsersTable1787573944791';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_46a570d4ae0901b4971840999ed"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" DROP CONSTRAINT "UQ_46a570d4ae0901b4971840999ed"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_46a570d4ae0901b4971840999ed" UNIQUE ("supabaseId")`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD CONSTRAINT "UQ_46a570d4ae0901b4971840999ed" UNIQUE ("supabaseId")`,
+    );
+  }
 }
