ba815ab222c7865e8abaf604184710395065928b
Chin Guang <ganchinguang0622@gmail.com>
2026-08-24 20:19:39 +0800

    feat: create migration for users table with unique constraint adjustment

---
+++ src/database/migrations/1787573944791-CreateUsersTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsersTable1787573944791 implements MigrationInterface {
+    name = 'CreateUsersTable1787573944791'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_46a570d4ae0901b4971840999ed"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_46a570d4ae0901b4971840999ed" UNIQUE ("supabaseId")`);
+    }
+
+}
